1be53e5247d500d4dfea733b86502882360dcd44
Quick-Genius <singhharshgood@gmail.com>
2025-06-19 17:48:56 +0530

    Tring to fix production issue

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-    origin: `${process.env.VITE_FRONTEND_URL}`, // your frontend application URL
+    origin: `${process.env.FRONTEND_URL}`, // your frontend application URL
     methods: ['GET', 'POST'],
     credentials: true,
     optionsSuccessStatus: 200
